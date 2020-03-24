bee9f94ad806624dbfc4340c44fc0222a2f9929b
Monika Bagyal <37674998+Minsifye@users.noreply.github.com>
2020-03-24 12:54:11 -0400

    chore: adding docstring to functions

--- data/process_data.py
+++ data/process_data.py
@@ -5,6 +5,12 @@ from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
+    """
+    load raw data from csv file, cleaning and transforming the data
+    Args:
+    messages_filepath: message csv file name
+    categories_filepath: categories csv file name
+    """
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
     df = pd.merge(messages, categories, how='inner', on='id') # merge datasets
@@ -38,11 +44,27 @@ def load_data(messages_filepath, categories_filepath):
 
 
 def save_data(df, database_filename):
+    """
+    save cleaned data to sql file
+    Args:
+    df: cleaned and transformed dataframe with messages and categories information
+    database_filename: Output DB file name along with filepath
+    """
     engine = create_engine('sqlite:///' + database_filename)
     df.to_sql('messages', engine, index=False, if_exists='replace')
 
 
 def main():
+    """
+    Main function to run the ETL pipeline
+    Args:
+    No arguments passed to this function, but this function checks for
+    4 arguments passed to script at run time.
+    Arg1:Default script name itself.
+    Arg2:messages_filepath - messages.csv file name and path
+    Arg3:categories_filepath - categories.csv file name and path
+    Arg4:database_filepath - database name with path.
+    """
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
